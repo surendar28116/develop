bdcdd6e5fe79920ff6a280254ad232e5d1c8fd1f
Surendar.D <surendar.d@e9ine.com>
2019-05-02 17:07:49 +0530

    test 10

--- app/src/main/java/com/e9ine/android/driver/module/AuthenticationModule/view/activity/LoginActivity.java
+++ app/src/main/java/com/e9ine/android/driver/module/AuthenticationModule/view/activity/LoginActivity.java
@@ -12,6 +12,6 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        //junkin 10
+        //junkin 12
     }
 }
