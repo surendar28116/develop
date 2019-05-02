4c9ff65ad997c19adaef29bf5e29135098fa6ed7
Surendar.D <surendar.d@e9ine.com>
2019-05-02 16:17:19 +0530

    jenkins

--- app/src/main/java/com/e9ine/android/driver/module/AuthenticationModule/view/activity/LoginActivity.java
+++ app/src/main/java/com/e9ine/android/driver/module/AuthenticationModule/view/activity/LoginActivity.java
@@ -12,6 +12,6 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        //junkins done
+        //junkins
     }
 }
