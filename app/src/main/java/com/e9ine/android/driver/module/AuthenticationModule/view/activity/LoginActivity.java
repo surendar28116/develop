0726910f530b81de518e96383e59c2bca1ab5660
Surendar.D <surendar.d@e9ine.com>
2019-05-02 18:58:16 +0530

    push success

--- app/src/main/java/com/e9ine/android/driver/module/AuthenticationModule/view/activity/LoginActivity.java
+++ app/src/main/java/com/e9ine/android/driver/module/AuthenticationModule/view/activity/LoginActivity.java
@@ -11,6 +11,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        //jinkin push success
+        //jinkin push success 1
     }
 }
