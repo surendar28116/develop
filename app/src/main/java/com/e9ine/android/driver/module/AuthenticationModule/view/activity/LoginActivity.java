39a45fcfc9409ee4f5982f7a95c6240ad0126055
Surendar.D <surendar.d@e9ine.com>
2019-05-02 17:39:53 +0530

    push 18

--- app/src/main/java/com/e9ine/android/driver/module/AuthenticationModule/view/activity/LoginActivity.java
+++ app/src/main/java/com/e9ine/android/driver/module/AuthenticationModule/view/activity/LoginActivity.java
@@ -12,6 +12,6 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        //junkin 18
+        //junkin push 18
     }
 }
